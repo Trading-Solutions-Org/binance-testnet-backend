7a4eb4882c611fe20d868f1e639e706756fcf6d4
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2024-02-02 19:27:20 +0200

    Feature: Console log body on trading-view alert

--- src/controllers/trading-view/trading-view-controller.ts
+++ src/controllers/trading-view/trading-view-controller.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/alert', (request) => {
-  console.log(request)
+  console.log(request.body)
 })
 
 export default router
